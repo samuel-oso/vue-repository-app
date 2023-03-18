517777fd45e3d39bd229fc7504f255659b83604c
samueloso <ososamuel035@gmail.com>
2023-03-18 19:08:08 +0100

    feat: error 404 page

--- src/pages/ErrorPage.vue
+++ src/pages/ErrorPage.vue
@@ -1,5 +1,13 @@
 <template>
-  <p>ErrorPage</p>
+  <div class="main">
+    <div class="singleRepo error">
+      <img src="../assets/ErrorImage.svg" alt="error" />
+      <h1>Page not found</h1>
+    </div>
+    <router-link to="/">
+      <div class="homePage_Route">Back to home page <span></span></div>
+    </router-link>
+  </div>
 </template>
 
 <script>
